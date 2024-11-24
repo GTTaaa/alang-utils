ece044c0a55d4c5218a564d8955c336912c356ae
郭超民 <1004969659@qq.com>
2024-11-24 21:26:58 +0800

    chore: prepare for npm publish

--- src/test/is.test.ts
+++ src/test/is.test.ts
@@ -1,7 +1,26 @@
 import { describe, expect, it } from 'vitest'
-import { isString, isNumber, isObject, isEmpty, isArray } from '../utils/is/is'
+import { isString, isNumber, isObject, isEmpty, isArray, is } from '../utils/is/is'
 
 describe('is utils', () => {
+  describe('is', () => {
+    it('应该正确判断各种类型', () => {
+      // 字符串类型判断
+      expect(is('hello', 'string')).toBe(true)
+      expect(is(123, 'string')).toBe(false)
+
+      // 数字类型判断
+      expect(is(123, 'number')).toBe(true)
+      expect(is('123', 'number')).toBe(false)
+
+      // 对象类型判断
+      expect(is({}, 'object')).toBe(true)
+      expect(is([], 'object')).toBe(false)
+
+      // 数组类型判断
+      expect(is([], 'array')).toBe(true)
+      expect(is({}, 'array')).toBe(false)
+    })
+  })
   describe('isString', () => {
     it('should return true for strings', () => {
       expect(isString('')).toBe(true)
--- src/utils/is/is.ts
+++ src/utils/is/is.ts
@@ -1,3 +1,16 @@
+/**
+ * 判断某个值是否为指定类型
+ * @param val 需要判断的值
+ * @param type 需要判断的类型
+ * @returns boolean 如果值为该类型返回 true，否则返回 false
+ */
+// export function is(val: unknown, type: string): boolean {
+//   return Object.prototype.toString.call(val).slice(8, -1).toLowerCase() === type.toLowerCase()
+// }
+export function is(val: unknown, type: string): boolean {
+  return Object.prototype.toString.call(val).toLowerCase() === `[object ${type}]`.toLowerCase()
+}
+
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isNumber = (val: unknown): val is number => typeof val === 'number'
 export const isBoolean = (val: unknown): val is boolean => typeof val === 'boolean'
